95eaf8536f0554d7ee847e63bbe7589dc1f5b6a4
peterpirogtf <peterpirogtf@gmail.com>
2020-08-16 22:39:57 +0200

    own tests

--- _own_test1.py
+++ _own_test1.py
@@ -1,12 +1,47 @@
+#https://www.e-consystems.com/blog/camera/how-to-access-cameras-using-opencv-with-python/
+"""
+0. CV_CAP_PROP_POS_MSEC Current position of the video file in milliseconds.
+1. CV_CAP_PROP_POS_FRAMES 0-based index of the frame to be decoded/captured next.
+2. CV_CAP_PROP_POS_AVI_RATIO Relative position of the video file
+3. CV_CAP_PROP_FRAME_WIDTH Width of the frames in the video stream.
+4. CV_CAP_PROP_FRAME_HEIGHT Height of the frames in the video stream.
+5. CV_CAP_PROP_FPS Frame rate.
+6. CV_CAP_PROP_FOURCC 4-character code of codec.
+7. CV_CAP_PROP_FRAME_COUNT Number of frames in the video file.
+8. CV_CAP_PROP_FORMAT Format of the Mat objects returned by retrieve() .
+9. CV_CAP_PROP_MODE Backend-specific value indicating the current capture mode.
+10. CV_CAP_PROP_BRIGHTNESS Brightness of the image (only for cameras).
+11. CV_CAP_PROP_CONTRAST Contrast of the image (only for cameras).
+12. CV_CAP_PROP_SATURATION Saturation of the image (only for cameras).
+13. CV_CAP_PROP_HUE Hue of the image (only for cameras).
+14. CV_CAP_PROP_GAIN Gain of the image (only for cameras).
+15. CV_CAP_PROP_EXPOSURE Exposure (only for cameras).
+16. CV_CAP_PROP_CONVERT_RGB Boolean flags indicating whether images should be converted to RGB.
+17. CV_CAP_PROP_WHITE_BALANCE Currently unsupported
+18. CV_CAP_PROP_RECTIFICATION Rectification flag for stereo cameras (note: only supported by DC1394 v 2.x backend currently)
 
+"""
+
+
+
+######################### READ WEBCAM  ############################
 import cv2
-cameraNumber=0
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FPS, 30)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
 
 
-fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-cap = cv2.VideoCapture()
-cap.open(cameraNumber + 1 + cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FOURCC, fourcc)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-cap.set(cv2.CAP_PROP_FPS, 60)
+#cap.set(3, frameWidth)
+#cap.set(4, frameHeight)
+#cap.set(10,150)
+while True:
+    success, img = cap.read()
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
--- chapter1.py
+++ chapter1.py
@@ -21,7 +21,7 @@
 import cv2
 frameWidth = 640
 frameHeight = 480
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10,150)
@@ -30,3 +30,5 @@ while True:
     cv2.imshow("Result", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+cap.release()
+cv2.destroyAllWindows()
